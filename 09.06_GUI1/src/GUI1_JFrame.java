f5359bb738ccecf8dac81031ad52ed988907c3aa
JiminIT <yhhs7741@naver.com>
2019-09-11 09:05:32 +0900

    가운데 정렬해보기

--- 09.06_GUI1/src/GUI1_JFrame.java
+++ 09.06_GUI1/src/GUI1_JFrame.java
@@ -1,8 +1,24 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class GUI1_JFrame {
     public static void main(String[] args) {
         JFrame frame = new JFrame("창이름");
+
+
+//        frame.setPreferredSize(new Dimension(400,200));
+//        frame.setLocation(300,300);
+
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int 화w = screenSize.width;
+        int 화h = screenSize.height;
+        int 창w = 400;
+        int 창h = 200;
+        frame.setLocation((화w-창w)/2,(화h-창h)/2);
+
+//        frame.setLocationRelativeTo(null);
+
+
         JPanel panel = new JPanel();
         JLabel label = new JLabel("아싸 어쩌다 금요일");
         JButton button = new JButton("오예");
