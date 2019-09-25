0516df23b334d475b192c187cc5f50203ea9bc01
JiminIT <yhhs7741@naver.com>
2019-09-25 09:28:12 +0900

    make showResult()

--- 09.06_GUI1/src/GUi2_Components.java
+++ 09.06_GUI1/src/GUi2_Components.java
@@ -80,32 +80,34 @@ public class GUi2_Components {
                }
            };
 
-           cb0.addItemListener(il);
-           rb0.addItemListener(il);
 
 
 
        public void actionPerformed(ActionEvent actionEvent){
-            String result = "";
-            result += lb0.getText();
-            result += bt0.getText();
-            result += cb0.isSelected();
-            result += cb1.isSelected();
-            result += rb0.isSelected();
-            result += rb1.isSelected();
-            result += tf0.getText();
-            result += ta0.getText();
-            result += cx0.getSelectedIndex();
-            result += cx0.getSelectedItem();
-            lbResult.cbox.getText(result);
-        }
+           showResult(lb0, bt0, cb0, cb1, rb0, rb1, tf0, ta0, cx0);
+       }
 
-        frame.add(panel);
+//        frame.add(panel);
+//
+//        frame.setPreferredSize(new Dimension(600,400));
+//        frame.pack();
+//        frame.setVisible(true);
+//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//    }
+//    }
 
-        frame.setPreferredSize(new Dimension(600,400));
-        frame.pack();
-        frame.setVisible(true);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
+    private static void showResult(JLabel lb0, JButton bt0, JCheckBox cb0, JCheckBox cb1, JRadioButton rb0, JRadioButton rb1, JTextField tf0, JTextArea ta0, JComboBox cx0) {
+        String result = "";
+        result += lb0.getText();
+        result += bt0.getText();
+        result += cb0.isSelected();
+        result += cb1.isSelected();
+        result += rb0.isSelected();
+        result += rb1.isSelected();
+        result += tf0.getText();
+        result += ta0.getText();
+        result += cx0.getSelectedIndex();
+        result += cx0.getSelectedItem();
+        lbResult.cbox.getText(result);
     }
 
