aac7c2d9522aeea679eafbe02a1b08cb74f56440
JiminIT <yhhs7741@naver.com>
2019-10-29 08:40:21 +0900

    su main smple

--- 09.06_GUI1/src/su/main.java
+++ 09.06_GUI1/src/su/main.java
@@ -4,14 +4,20 @@ import javax.swing.*;
 import java.awt.*;
 
 public class main {
-    public static void main(String ar[]){
+    public static void main(String ar[]) {
         JFrame frame = new JFrame();
         JPanel panel = new JPanel();
 
+        JTextField tf = new JTextField();
+
+        frame.add(tf);
         frame.add(panel);
 
-        frame.setPreferredSize(new Dimension(600,400));
+        frame.setPreferredSize(new Dimension(600, 400));
         frame.pack();
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+    }
 }
