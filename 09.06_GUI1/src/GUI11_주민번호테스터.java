28152c444d0b8cff54c7210f0834b7e37c0e4c61
JiminIT <yhhs7741@naver.com>
2019-10-31 02:50:26 +0900

    주민번호 테스터

--- 09.06_GUI1/src/GUI11_주민번호테스터.java
+++ 09.06_GUI1/src/GUI11_주민번호테스터.java
@@ -4,31 +4,34 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class GUI11_주민번호테스터 {
-
-    public static void main(String ar[]){
-        JFrame frame = new JFrame();
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Components");
         JPanel panel = new JPanel();
+        JTextField jm0 = new JTextField(10);
         JLabel lbMinus = new JLabel("-");
         JTextField jm1 = new JTextField(10);
         JButton btTest = new JButton("테스트");
-        JTextField jm0 = new JTextField(10);
-
         btTest.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String jm = jm0.getText() + jm1.getText();
-                if(isGood(jm)){
-                    lbMinus.setText("o");//jm0,jm1 주민번호 가져오기
-                }
-                else {
-                    //맞는지 확인
-                    //맞으면 =0
-                    //아니면 x
-                    lbMinus.setText("x");
+                //jm0, jm1에 주민번호 가져오기
+                String jm = jm0.getText()+jm1.getText();
+
+                //맞는지 확인하자
+                if(isGood(jm)==true) { //isGood(jm)
+                    //맞으면 lbMinus = "O"
+                    lbMinus.setText("O");
+                }else{
+                    //틀리면 lbMinus = "X"
+                    lbMinus.setText("X");
                 }
             }
         });
 
+        panel.add(jm0);
+        panel.add(lbMinus);
+        panel.add(jm1);
+        panel.add(btTest);
         frame.add(panel);
 
         frame.setPreferredSize(new Dimension(600,400));
@@ -38,12 +41,15 @@ public class GUI11_주민번호테스터 {
     }
 
     private static boolean isGood(String jm) {
+        int[] mulN = {2,3,4,5,6,7,8,9,2,3,4,5};
         int sum=0;
-        int[] muin = {2,3,4,5,6,7,8,9,2,3,4,5};
-        for(int i =0; i <jm.length()- ; i++){
-            sum += (jm.charAt(i) - '0') = muin[i];
+        for(int i = 0;i<jm.length()-1;i++){
+            sum+=(jm.charAt(i) - '0')*mulN[i];
         }
-        int last = 11-(sum%11);
+        //System.out.println(sum);
+
+        int last = (11 - (sum % 11)) % 10;
+
         return (jm.charAt(jm.length()-1)-'0') == last;
     }
 }
