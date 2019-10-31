dba687eba1d341df65732ac78c369d95c28b00b4
JiminIT <yhhs7741@naver.com>
2019-11-01 01:12:25 +0900

    Personal Color 프로젝트

--- 09.06_GUI1/src/su/question1.java
+++ 09.06_GUI1/src/su/question1.java
@@ -3,7 +3,7 @@ package su;
 import javax.swing.*;
 
 public class question1 extends main{
-
+// 퀴즈 문제랑 선택값 들어가는 변수 넣기.
     JButton qbt1_1 = new JButton();
     JButton qbt1_2 = new JButton();
     JButton qbt1_3 = new JButton();
